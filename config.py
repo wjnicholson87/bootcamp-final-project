9b446becc08364a09be4676a55db6aa991fad0d7
LukeSnywalker <lukesnyder98@gmail.com>
2022-08-20 13:14:30 -0400

    Update config with start.gg API key

--- config.py
+++ config.py
@@ -1,5 +1,2 @@
-# Open Weather API Key
-weather_api_key="c6fcbdc1aa7d15d105b73f3c47b9dc28"
-
-# Google API Key
-g_key = "AIzaSyDDLwrJ3wjZZuAVf1Z-yh1QoXphYlI_nj0"
+# start.gg API key
+key = '29c3c9a87e30d8a2f14aa4456cb691a1'
